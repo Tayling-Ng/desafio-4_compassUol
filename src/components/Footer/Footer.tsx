b4d58dd2cc546e37e98c15f551e0a136459e4eb0
Tayling <taymng@yahoo.com.br>
2025-02-10 16:27:04 -0300

    fix: Item positioning

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -6,26 +6,27 @@ import linkedin from '../../assets/img-footer/linkedin.png';
 export const Footer = () => {
     return (
         //footer
-        <div className="flex justify-center items-center h-[505px] border-t border-black/[0.17] mt-[4330px] font-poppin">
+        <div className="flex justify-center items-center h-[505px] border-t border-black/[0.17] font-poppins">
             {/* Group 45 */}
             <div className="flex flex-col w-[1240.01px] h-[419px] left-[100px] gap-[100px]">
                 {/* Group 32 */}
-                <div className="flex flex-row justify-between items-center gap-[100px]">
+                <div className="flex flex-row justify-between ">
                     {/* Infos e Redes Sociais */}
                     <div className="flex flex-col gap-[50px]">
                         <div>
-                            <h1 className="w-[85px] h-[36px] text-2xl font-bold leading-[36px] text-left text-black">
+                            <h1 className="w-[85px] h-[36px] text-2xl font-bold leading-[36px] text-black">
                                 Furniro.
                             </h1>
                         </div>
                         <div>
-                            <p className="w-[285px] h-[72px] text-base font-normal leading-[24px] text-left text-[#9F9F9F]">
+                            <p className="w-[285px] h-[72px] text-base font-normal leading-[24px] text-[#9F9F9F]">
                                 400 University Drive Suite 200 Coral
                                 <br /> Gables,
                                 <br />
                                 FL 33134 USA
                             </p>
                         </div>
+                        {/* Group 12 */}
                         <div className="flex flex-row justify-between w-[184px] h-[34px]">
                             <a href="https://www.facebook.com/" target="_blank">
                                 <img
@@ -62,16 +63,16 @@ export const Footer = () => {
                     {/* Group 31 */}
                     <div className="flex flex-row justify-between w-[710px] h-[312px] left-[523.01px]">
                         {/* Group 28 */}
-                        <div className="flex flex-row justify-between">
+                        <div className="flex flex-row justify-center gap-[114px]">
                             {/* Group 26 */}
                             <div>
-                                <div>
+                                <div className="flex flex-col gap-[55px]">
                                     {/* Links */}
-                                    <div>
+                                    <div className="text-[#9F9F9F]">
                                         <p>Links</p>
                                     </div>
                                     {/* Group 24 */}
-                                    <div>
+                                    <div className="flex flex-col gap-[46px]">
                                         <p>Home</p>
                                         <p>Shop</p>
                                         <p>About</p>
@@ -81,13 +82,13 @@ export const Footer = () => {
                             </div>
                             {/* Group 27 */}
                             <div>
-                                <div>
+                                <div className="flex flex-col gap-[55px]">
                                     {/* Help */}
-                                    <div>
+                                    <div className="text-[#9F9F9F]">
                                         <p>Help</p>
                                     </div>
                                     {/* Group 25 */}
-                                    <div>
+                                    <div className="flex flex-col gap-[46px]">
                                         <p>Payment Options</p>
                                         <p>Returns</p>
                                         <p>Privacy Policies</p>
@@ -96,13 +97,13 @@ export const Footer = () => {
                             </div>
                         </div>
                         {/* Group 30 */}
-                        <div>
+                        <div className="flex flex-col gap-[45px]">
                             {/* Newsletter */}
-                            <div>
+                            <div className="text-[#9F9F9F]">
                                 <p>Newsletter</p>
                             </div>
                             {/* Group 29 */}
-                            <div className="flex flex-row justify-between">
+                            <div className="flex flex-row justify-between gap-[11px]">
                                 <input
                                     type="email"
                                     placeholder="Enter Your Email Address"
@@ -116,10 +117,10 @@ export const Footer = () => {
                     </div>
                 </div>
                 {/* Group 44 */}
-                <div className="items-end w-[1240px] h-[59px] border-t border-[#D9D9D9]">
-                    <p className="flex items-end h-[24px] border-t border-t-solid border-t-[#D9D9D9] text-base font-normal leading-[24px] text-left">
-                        2023 furniro. All rights reverved
-                    </p>
+                <div className="relative w-[1240.0145px] border-t-[1px] border-solid  border-[#D9D9D9]">
+                    <div className="flex border-t border-t-solid text-base leading-[24px] text-left">
+                        <p>2023 furniro. All rights reverved</p>
+                    </div>
                 </div>
             </div>
         </div>
