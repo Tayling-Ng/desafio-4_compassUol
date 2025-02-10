5276f4516c96a97d7145f2ab198ea6bd0f11bd6c
Tayling <taymng@yahoo.com.br>
2025-02-10 16:38:47 -0300

    chore: Home Page Structure

--- src/pages/homePage/homePage.tsx
+++ src/pages/homePage/homePage.tsx
@@ -1,7 +1,17 @@
+import { MainSection } from './MainSection/MainSection.tsx';
+import { Section2 } from './Section2/Section2.tsx';
+import { Section3 } from './Section3/Section3.tsx';
+import { Section4 } from './Section4/Section4.tsx';
+import { Section5 } from './Section5/Section5.tsx';
+
 export const HomePage = () => {
     return (
-        <div className="w-full flex flex-col">
-            <p className={`text-5xl text-center text-red-500`}>texto</p>
+        <div>
+            <MainSection />
+            <Section2 />
+            <Section3 />
+            <Section4 />
+            <Section5 />
         </div>
     );
 };
